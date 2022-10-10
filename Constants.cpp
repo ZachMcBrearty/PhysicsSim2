13c0b3f14f8b698167e73e762ece271004ada00b
ZachMcBrearty <mcbreartyzg@gmail.com>
2022-10-10 19:40:06 +0100

    Removed excess files

--- Constants.cpp
+++
@@ -1,9 +0,0 @@
-#include<math.h>
-#include<iostream>
-#include<fstream>
-
-const double G = 1; // 6.67 * pow(10, -11);
-const long long scale = 10; //5 * pow(10, 18); //2 * pow(10, 11)
-const int maxtimestep = 0.001;
-const int maxtime = 10;
-int stepcount = 10000 + 1;
